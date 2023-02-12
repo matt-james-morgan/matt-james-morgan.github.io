beddcfff1ff9a25e7bfd4c8f9d5488656952a362
matt-james-morgan~ <matt.james.morgan97@gmail.com>
2023-02-11 22:07:03 -0500

    made changes to how the letters wiggle

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ const menuCloseHam = document.querySelector('#menu-button-ham');
 const hamColor = document.querySelectorAll('.ham-menu span');
 const hamMenu = document.querySelector('.ham-menu');
 const sidebar = document.getElementById('sidebar');
+const menu = document.getElementById('menu');
 
 //Open side menu in fullscreen
  menuTitle.addEventListener('click',()=>{
-    document.getElementById('menu').style.left = '0px';
-    menuTitle.style.left = '-10rem';
-    sidebar.style.width = '300px';
+    menu.style.left = '0px';
+    menuTitle.style.width = '0';
+    menu.style.width = '300px';
 });
 
 //close side menu in fullscreen
 menuClose.addEventListener('click', ()=>{
-    document.getElementById('menu').style.left = '-300px';
-    menuTitle.style.left = '0';
-    sidebar.style.width = '8%';
+    menu.style.left = '-300px';
+    menuTitle.style.width = '100%';
+    
 });
 
 //This loops through the array of span elements of hamburger menu to change their colour to red
@@ -34,18 +35,12 @@ hamMenu.addEventListener('mouseout', ()=>{
 
 //On click pulls out side menu in tablet view
 hamMenu.addEventListener('click',()=>{
-    document.getElementById('menu').style.left = '0px';
-    
-    hamMenu.style.left = '-30rem';
-    
-  
-    
+    menu.style.left = '0px';
+    hamMenu.style.left = '-20%';
 });
 
 menuCloseHam.addEventListener('click', ()=>{
         document.getElementById('menu').style.left = '-300px'; 
-        sidebar.style.width = '8%';
-        sidebar.style.minWidth = '8%';
         hamMenu.style.left ='-5%';
 });
 
